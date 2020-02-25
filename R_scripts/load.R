bbc0896414c8d6d4fbc023d4df41837fdf589da1
gracepratt <gbepratt@berkeley.edu>
2020-02-24 22:37:41 -0800

    importing data

---
+++ R_scripts/load.R
@@ -0,0 +1,36 @@
+########################################################################
+## load packages and data for 2018 and 2017 field research study
+########################################################################
+
+
+
+
+########################################################################
+## load data
+########################################################################
+
+# 2017 dataset
+data2017 <- read.csv("~/data/2017-Complete.csv")
+
+# 2018 dataset
+data2018 <- read.csv("~/data/2018-Complete.csv")
+
+# load plant traits table
+plantID <- read.csv("~/data/PlantID.csv")
+
+# OTU tables
+otu <- read.csv("~/data/fungiOTUcsv")
+
+# taxanomy list 
+tax <- read.csv('~/data/taxonomy.csv')
+
+
+
+
+
+
+
+
+
+
+
