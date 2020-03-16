52d4d357425d406a85e51a7c2b4f04958bbb9557
gracepratt <gbepratt@berkeley.edu>
2020-03-15 23:16:38 -0700

    changed mantel to function

--- R Scripts/analysis.R
+++ R Scripts/analysis.R
@@ -13,16 +13,16 @@ summary(all_farms_model)
 #gdm.varImp(test_gdm_table, geo = TRUE, nPerm = 10) #very slow
 
 #gdm plot
-plot(gdmModel, plot.layout = c(1,2))
+plot(all_farms_model, plot.layout = c(1,2))
 
 #monocultures
 mono_model <- gdmModel(mono_inputs)
-summary(mono_model)
+#summary(mono_model)
 table(mono_model)
 
 #polycultures
 poly_model <- gdmModel(poly_inputs)
-summary(poly_model)
+#summary(poly_model)
 table(poly_model)
 
 
@@ -44,41 +44,8 @@ summary(amfModel)
 ## mantel tests
 ########################################################################
 
-
-# transformed OTU table 
-h <- decostand(species.rr_df, method = "hellinger")
-pa <- decostand(species.rr_df, method = "pa")
-
-# CREATE DISSIMILARITY MATRIX
-dist.h <- as.matrix((vegdist(h, "bray")))
-
-dist.pa <- as.matrix((vegdist(pa, "bray")))
-
-#dist.envi <- as.matrix((vegdist(envi_table, "bray")))
-
-dist.envi <- as.matrix(dist(envi_table, method = "euclidean"))
-
-#geo table
-geo <- wo_mock %>% dplyr::select("Long_point", "Lat_point")
-
-dist.geo <- distm(geo, fun = distHaversine) #not sure if I should be using this
-
-
-#abundace vs. environment
-
-mantel_envi_h <- mantel(dist.h, dist.envi, method = "spearman")
-
-mantel_envi_pa <- mantel(dist.pa, dist.envi)
-
-
-#abundance vs. geography
-
-mantel(dist.h, dist.geo, method = "spearman")
-
-mantel(dist.pa, dist.geo)
-
-
-
-
+all_mantel <- mantel_func(all_inputs)
+mono_mantel <- mantel_func(mono_inputs)
+poly_mantel <- mantel_func(poly_inputs)
 
 
