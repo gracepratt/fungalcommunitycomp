d5f05b59957fb59aa0315dfd21e254567915cb4f
gracepratt <gbepratt@berkeley.edu>
2020-03-19 11:40:44 -0700

    making plot functions

--- R Scripts/plots.R
+++ R Scripts/plots.R
@@ -2,37 +2,43 @@
 ## plotting
 ########################################################################
 
-#custom plots
+########################################################################
+## predictor coefficient plots
+########################################################################
 
-#4 predictors 
-isplines <- isplineExtract(gdmModel)
+predictors_plot <- function(model){
+  isplines <- isplineExtract(model)
+  
+  x_values <- data.frame(isplines$x) %>%
+    lapply(function(x) scale(x, center = TRUE)) %>% 
+    as.data.frame() %>% 
+    add_column(Key = row.names(data.frame(isplines$x))) %>%
+    dplyr::select("Key", X = 1)
+  
+  y_values <- data.frame(isplines$y) %>% 
+    add_column(Key = row.names(data.frame(isplines$y))) %>%
+    gather(key = "Factor", value = "Y", -"Key" )
+  
+  predictors <- y_values %>%
+    full_join(x_values, by = "Key")
+  
+  
+  predictors %>% ggplot(aes(x = X, y = Y, color = Factor)) +
+    geom_line() +
+    xlab("Predictor Dissimilarity") +
+    ylab("Partial Ecological Distance")
+  
+}
 
-x_values <- data.frame(isplines$x) %>%
-  lapply(function(x) scale(x, center = TRUE)) %>% 
-  as.data.frame() %>% 
-  add_column(number = c(1:200)) 
 
-y_values <- data.frame(isplines$y) %>% 
-  add_column(number = c(1:200)) %>%
-  rename(Geographic_y = Geographic, pH_y = pH, OM_y = OM, P_y = P)
+########################################################################
+## predicted vs observed compositional dissimilarity
+########################################################################
 
-four_predictors <- x_values %>%
-  join(y_values)
-
-four_predictors %>% ggplot() +
-  geom_line(aes(x = Geographic, y = Geographic_y)) +
-  geom_line(aes(x = pH, y = pH_y)) +
-  geom_line(aes(x = OM, y = OM_y)) +
-  geom_line(aes(x = P, y = P_y)) +
-  xlab("Predictor Dissimilarity") +
-  ylab("Partial Ecological Distance")
-
-#predicted vs observed compositional dissimilarity
-
-comp_df <- data.frame(gdmModel$predicted, gdmModel$observed)
+comp_df <- data.frame(all_farms_model$predicted, all_farms_model$observed)
 
 comp_df %>% ggplot(aes(x = gdmModel.predicted, y = gdmModel.observed)) +
-  geom_point(color = 'lightblue') +
+  geom_point() +
   geom_smooth(method = lm)
 
 #ecological dist vs observed compositional dissimilarity
